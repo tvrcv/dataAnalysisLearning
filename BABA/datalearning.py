0e8d16f54b6809d9ac121044e20fca65a0b09687
Tomas Vargas <tomas13@vt.edu>
2022-11-01 20:07:33 +0100

    first commit

---
+++ BABA/datalearning.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+
+ 
+df = pd.read_csv("/Users/tovare/dataAnalysisLearning/BABA/BABA.csv")
+
+#print(df.describe())
+
+
+#print(df.tail())
+#2022-11-01
+
+#print(df.head())
+#2021-11-01
